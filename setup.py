0f60d4f1a940e8a63372a6f0e3205215025b1d8b
Romain FLIEDEL <romain.fliedel@gmail.com>
2019-11-12 00:16:42 +0100

    rename to tsdemux

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tsdemux-r0ro",
+    name="tsdemux",
     version="0.0.1",
     author="Romain Fliedel",
     author_email="romain.fliedel@gmail.com",
